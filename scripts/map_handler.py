3e614d92f1df51d033993a55b34520f64c3c95f1
Magdamini <mpabisz@student.agh.edu.pl>
2024-05-07 01:22:56 +0200

    Honk on the map!

--- scripts/map_handler.py
+++ scripts/map_handler.py
@@ -1,6 +1,7 @@
 from scripts.tilemap import Tilemap
 import scripts.items as items
 from scripts.utils import load_json
+from scripts.level_manager import MAX_LVL
 import pygame
 
 MAP_CHANGE_TILES = {"0": [((9, 14), "1", (16, -40))],
@@ -9,8 +10,8 @@ MAP_CHANGE_TILES = {"0": [((9, 14), "1", (16, -40))],
                     "3": [((-0.5, 11), "2", (1224, 464)), ((129.5, 11), "4", (-40, 160))],
                     "4": [((-3.5, 10), "3", (2056, 176)), ((39.5, 11), "5", (8, 352)), ((13, 4.5), "4a", (48, 200))],
                     "4a": [((3, 13.5), "4", (208, 88))],
-                    "5": [((-0.5, 22), "4", (616, 176)), ((23, 2.5), "5a", (160, 280))],
-                    "5a": [((10, 18.5), "5", (368, 56))]}
+                    "5": [((-0.5, 22), "4", (616, 176)), ((23, 3.5), "5a", (160, 280))],
+                    "5a": [((10, 18.5), "5", (368, 72))]}
 
 ITEM_TYPES = {"00": items.Plaster,
               "01": items.Bandage,
@@ -43,7 +44,9 @@ class MapHandler():
         
         self.load_items()
         
-        self.curr_map = "0"
+        
+        # self.curr_map = "0"
+        self.curr_map = "5"
         self.tile_size = self.get_curr_map().tile_size
         self.player = player
         
@@ -52,13 +55,14 @@ class MapHandler():
         return self.maps[self.curr_map]
     
 
-    def change_map(self):
+    def change_map(self, curr_lvl):
         player_rect = self.player.rect()
         
         for loc, new_map, new_pos in MAP_CHANGE_TILES.get(self.curr_map, ()):
             door_rect = pygame.Rect(loc[0] * self.tile_size, loc[1] * self.tile_size, self.tile_size, self.tile_size)
             
             if player_rect.colliderect(door_rect):
+                if new_map == '5a' and curr_lvl != MAX_LVL: return
                 self.curr_map = new_map
                 self.player.pos = list(new_pos)
                 self.game.tilemap = self.get_curr_map()
